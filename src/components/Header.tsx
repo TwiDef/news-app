2dc293a1dcfcf53f30c9adc3df55ae13a0b2481d
Def <deferenseo@gmail.com>
2025-04-17 20:37:52 +0300

    formatTitle(time of day)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import { formatDate } from '../helpers/formatDate';
 import { useGetCoords } from '../hooks/useGetCoords';
 import { useGetWeather } from '../hooks/useGetWeather';
+import { formatTitle } from '../helpers/formatTitle';
+import { formatDate } from '../helpers/formatDate';
 
 const Header: React.FC = () => {
   const { position, success, fail } = useGetCoords()
-  console.log(position, success, fail)
+  /* console.log(position, success, fail) */
 
   const { weatherInfo } = useGetWeather(position?.coords)
-  console.log(weatherInfo)
+  /* console.log(weatherInfo) */
+
+  const now = new Date()
 
   return (
     <header
       style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
       <div>
-        <h1>Good morning</h1>
-        <p>{formatDate(new Date)}</p>
+        <h1>{formatTitle(now)}</h1>
+        <p>{formatDate(now)}</p>
       </div>
 
       <div>
