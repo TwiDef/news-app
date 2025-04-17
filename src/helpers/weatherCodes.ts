af4cd660c64eb698447a21f25d3320a2b8b90b82
Def <deferenseo@gmail.com>
2025-04-17 20:16:25 +0300

    done weatherCodes

--- src/helpers/weatherCodes.ts
+++ src/helpers/weatherCodes.ts
@@ -1,6 +1,6 @@
 import { IweatherCodes } from "../@types";
 
-export const weatherCodes: Array<IweatherCodes> = [
+export const weatherCodes: ReadonlyArray<IweatherCodes> = [
   {
     code: 0,
     status: "clear sky",
@@ -81,4 +81,64 @@ export const weatherCodes: Array<IweatherCodes> = [
     status: "freezing rain",
     img: "https://cdn-icons-png.flaticon.com/512/18385/18385609.png"
   },
+  {
+    code: 71,
+    status: "light snow",
+    img: "https://cdn-icons-png.flaticon.com/512/4834/4834727.png"
+  },
+  {
+    code: 73,
+    status: "snow",
+    img: "https://cdn-icons-png.flaticon.com/512/17858/17858667.png"
+  },
+  {
+    code: 75,
+    status: "heavy snow",
+    img: "https://cdn-icons-png.flaticon.com/512/9755/9755252.png"
+  },
+  {
+    code: 77,
+    status: "snow grains",
+    img: "https://cdn-icons-png.flaticon.com/512/13396/13396889.png"
+  },
+  {
+    code: 80,
+    status: "light showers",
+    img: "https://cdn-icons-png.flaticon.com/512/1959/1959317.png"
+  },
+  {
+    code: 81,
+    status: "showers",
+    img: "https://cdn-icons-png.flaticon.com/512/8841/8841317.png"
+  },
+  {
+    code: 82,
+    status: "heavy showers",
+    img: "https://cdn-icons-png.flaticon.com/512/2864/2864448.png"
+  },
+  {
+    code: 85,
+    status: "light snow showers",
+    img: "https://cdn-icons-png.flaticon.com/512/6363/6363108.png"
+  },
+  {
+    code: 86,
+    status: "snow showers",
+    img: "https://cdn-icons-png.flaticon.com/512/7334/7334205.png"
+  },
+  {
+    code: 95,
+    status: "thunderstorm",
+    img: "https://cdn-icons-png.flaticon.com/512/1959/1959321.png"
+  },
+  {
+    code: 96,
+    status: "light thunderstorms with hail",
+    img: "https://cdn-icons-png.flaticon.com/512/1959/1959334.png"
+  },
+  {
+    code: 99,
+    status: "thunderstorm with hail",
+    img: "https://cdn-icons-png.flaticon.com/512/9211/9211942.png"
+  }
 ]
