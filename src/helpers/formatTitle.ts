2de4bc50e72d07c01d83a5c565bf230da7749783
Def <deferenseo@gmail.com>
2025-04-26 12:11:03 +0300

    fixed header daytime greetings

--- src/helpers/formatTitle.ts
+++ src/helpers/formatTitle.ts
@@ -4,7 +4,7 @@ export const formatTitle = (now: Date): string => {
   if (hours > 5 && hours < 12) {
     return "Good Morning"
 
-  } else if (hours > 12 && hours < 17) {
+  } else if (hours >= 12 && hours < 17) {
     return "Good Afternoon"
 
   } else if (hours > 17 && hours < 21) {
---
+++ src/services/currentsApi.ts
@@ -0,0 +1,2 @@
+const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+
