7cd078870d844137f0f54a458ae8020cc936c2f0
Def <deferenseo@gmail.com>
2025-06-21 13:23:48 +0300

    fix latest-news display(grid)

--- src/components/BlockLatestNews/BlockLatestNews.tsx
+++ src/components/BlockLatestNews/BlockLatestNews.tsx
@@ -6,6 +6,8 @@ import Container from '../Container';
 import PublishedInfo from '../PublishedInfo';
 import ErrorBanner from '../ErrorBanner';
 
+import './BlockLatestNews.css';
+
 type BlockLatestNewsProps = {}
 
 const BlockLatestNews: React.FC<BlockLatestNewsProps> = () => {
@@ -13,7 +15,8 @@ const BlockLatestNews: React.FC<BlockLatestNewsProps> = () => {
 
   if (isError) {
     return (
-      <ul className="px-8 grid grid-cols-3 justify-self-center gap-x-3 gap-y-10 mt-17 max-h-full overflow-y-auto opacity-70">
+      <ul
+        className="latest__wrapper px-8 gap-x-3 gap-y-10 mt-17 overflow-y-auto opacity-70">
         {new Array(30).fill("empty-error-item").map((_, i: number) => {
           return (
             <li key={i}>
@@ -29,7 +32,7 @@ const BlockLatestNews: React.FC<BlockLatestNewsProps> = () => {
 
   if (isLoading) {
     return (
-      <ul className="px-8 grid grid-cols-3 justify-self-center gap-x-3 gap-y-10 mt-17 max-h-full overflow-y-auto">
+      <ul className="latest__wrapper px-8 gap-x-3 gap-y-10 mt-17 max-h-full overflow-y-auto">
         {new Array(30).fill("empty-error-item").map((_, i: number) => {
           return (
             <li key={i}>
@@ -48,7 +51,7 @@ const BlockLatestNews: React.FC<BlockLatestNewsProps> = () => {
 
   return (
     <Container marginAuto={false}>
-      <ul className="pb-6 grid grid-cols-3 justify-self-center gap-x-3 gap-y-4 mt-17 max-h-full overflow-y-auto">
+      <ul className="latest__wrapper pb-6 gap-x-3 gap-y-10 mt-17 max-h-full overflow-y-auto">
         {data?.news.map((item: Inew) => {
           return (
             <li key={item.id}>
